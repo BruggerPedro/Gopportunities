0a56eed834085ea49e4f1ef0599d62ed00d2a4aa
BruggerPedro <pedromurilobrugger@hotmail.com>
2023-10-02 14:35:12 -0300

    feat: add update handler logic

--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -3,11 +3,44 @@ package handler
 import (
 	"net/http"
 
+	"github.com/BruggerPedro/Gopportunities.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func UpdateOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+func UpdateOpeningHandler(ctx *gin.Context) {
+	request := UpdateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+	// Update opening
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	// Save opening
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+	sendSuccess(ctx, "update-opening", opening)
 }
